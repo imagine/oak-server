2da53588fd0a3c79fbd1e3ad80381a95e94a7334
Josh Benjamin <imagine@jbenjamin.org>
2016-03-03 18:17:43 -0800

    Move app.js from cloud code

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var moment = require('moment');
+var _ = require('underscore');
+var md5 = require('cloud/libs/md5.js');
+var Buffer = require('buffer').Buffer;
+
+var responsesController = require(__dirname + 'cloud/controllers/responses.js');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 
@@ -27,10 +33,82 @@ var app = express();
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
-// Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a web site.');
+
+// The information showed about the poster
+var userEmail = 'hello@oakvideos.com';
+var userDisplayName = 'Oak Videos';
+var userDescription = 'Viewer';
+
+// Global app configuration section
+app.set('views', 'cloud/views');  // Specify the folder to find templates
+app.set('view engine', 'ejs');    // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
+
+// You can use app.locals to store helper methods so that they are accessible
+// from templates.
+app.locals._ = _;
+app.locals.hex_md5 = md5.hex_md5;
+app.locals.userEmail = userEmail;
+app.locals.userDisplayName = userDisplayName;
+app.locals.userDescription = userDescription;
+app.locals.formatTime = function(time) {
+  return moment(time).format('MMMM Do YYYY, h:mm a');
+};
+// Generate a snippet of the given text with the given length, rounded up to the
+// nearest word.
+app.locals.snippet = function(text, length) {
+  if (text.length < length) {
+    return text;
+  } else {
+    var regEx = new RegExp("^.{" + length + "}[^ ]*");
+    return regEx.exec(text)[0] + "...";
+  }
+};
+
+///////////////////////////
+// Routes
+///////////////////////////
+
+// RESTful routes for the blog response object.
+app.get('/responses', function(req, res) {
+  res.render('hello', { message: "Do you know which response you wanted to watch?" });
 });
+app.get('/responses/:id', responsesController.show);
+
+app.get('/u/:id', responsesController.showUser);
+
+app.get('/u', function(req, res) {
+  res.render('hello', { message: "Do you know which user's responses you wanted to watch?" });
+});
+
+// DEBUG
+// Show all responses on homepage
+// app.get('/debug', responsesController.index);
+app.get('/debug', function(req, res) {
+	var un = 'oakadmin';
+	var pw = '#zQ87xbNsV#GBiL';
+    if(un == undefined && pw == undefined) { res.end(); return; }
+    if(!req.headers['authorization']){
+        res.writeHead(401, {'WWW-Authenticate': 'Basic realm="Secure Area"', 'Content-Type': 'text/plain'});
+        res.end("You must have credentials for this page");
+        return;
+    }
+    var header=req.headers['authorization']||'',        // get the header
+        token = header.split(/\s+/).pop()||'',            // and the encoded auth token
+        auth = new Buffer(token, 'base64').toString(),    // convert from base64
+        parts = auth.split(/:/),                          // split on colon
+        username = parts[0],
+        password = parts[1];
+    if(username != un || password != pw){
+        res.writeHead(401, {'WWW-Authenticate': 'Basic realm="Secure Area"', 'Content-Type': 'text/plain'});
+		res.end("Incorrect username or password");
+    }
+    else {
+    	res.statusCode = 200;
+    	responsesController.index(req, res);
+    }
+});
+
 
 var port = process.env.PORT || 1337;
 app.listen(port, function() {
